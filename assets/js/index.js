66a56c90a6e23d0f712ec846678ed0660ee16221
Shilpa Koorayil <shilpakoorayil@gmail.com>
2025-11-14 14:41:20 +0530

    cssprop

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+
+
+
+// toggle:
+
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
+// .......................................
+let user = localStorage.getItem("buzzyUser");
+
+if (user) {
+  document.getElementById("welcomeUser").innerHTML = "Welcome, " + user + " 👋";
+  document.getElementById("logoutBtn").style.display = "inline-block";
+   document.getElementById("loginBtn").style.display = "none";
+
+
+}else{
+   document.getElementById("logoutBtn").style.display = "none";
+
+}
+
+// Auto Logout after 10 minutes
+setTimeout(() => {
+  localStorage.removeItem("buzzyUser");
+  window.location.href = "login.html";
+}, 600000); // 10 mins
+
+// Manual Logout
+document.getElementById("logoutBtn").onclick = function() {
+  localStorage.removeItem("buzzyUser");
+  localStorage.removeItem("isAdmin");
+  window.location.href = "login.html";
+};
+
